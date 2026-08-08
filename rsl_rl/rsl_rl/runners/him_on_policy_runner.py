232cced7a8ed89c66f2a3dc0960ccb5a6f7adf77
Jingchao She <163161756+et26-sjc@users.noreply.github.com>
2026-08-08 23:02:33 +0800

    Add optional W&B logging to HIM runner

--- rsl_rl/rsl_rl/runners/him_on_policy_runner.py
+++ rsl_rl/rsl_rl/runners/him_on_policy_runner.py
@@ -36,6 +36,11 @@ import statistics
 from torch.utils.tensorboard import SummaryWriter
 import torch
 
+try:
+    import wandb
+except ImportError:
+    wandb = None
+
 from rsl_rl.algorithms import PPO, HIMPPO
 from rsl_rl.modules import HIMActorCritic
 from rsl_rl.env import VecEnv
@@ -49,46 +54,100 @@ class HIMOnPolicyRunner:
                  log_dir=None,
                  device='cpu'):
 
-        self.cfg=train_cfg["runner"]
+        self.train_cfg = train_cfg
+        self.cfg = train_cfg["runner"]
         self.alg_cfg = train_cfg["algorithm"]
         self.policy_cfg = train_cfg["policy"]
         self.device = device
         self.env = env
         if self.env.num_privileged_obs is not None:
-            num_critic_obs = self.env.num_privileged_obs 
+            num_critic_obs = self.env.num_privileged_obs
         else:
             num_critic_obs = self.env.num_obs
         self.num_actor_obs = self.env.num_obs
         self.num_critic_obs = num_critic_obs
         actor_critic_class = eval(self.cfg["policy_class_name"]) # HIMActorCritic
-        actor_critic: HIMActorCritic = actor_critic_class( self.env.num_obs,
-                                                        num_critic_obs,
-                                                        self.env.num_one_step_obs,
-                                                        self.env.num_actions,
-                                                        **self.policy_cfg).to(self.device)
+        actor_critic: HIMActorCritic = actor_critic_class(self.env.num_obs,
+                                                         num_critic_obs,
+                                                         self.env.num_one_step_obs,
+                                                         self.env.num_actions,
+                                                         **self.policy_cfg).to(self.device)
         alg_class = eval(self.cfg["algorithm_class_name"]) # HIMPPO
         self.alg: HIMPPO = alg_class(actor_critic, device=self.device, **self.alg_cfg)
         self.num_steps_per_env = self.cfg["num_steps_per_env"]
         self.save_interval = self.cfg["save_interval"]
 
         # init storage and model
-        self.alg.init_storage(self.env.num_envs, self.num_steps_per_env, [self.env.num_obs], [self.env.num_privileged_obs], [self.env.num_actions])
+        self.alg.init_storage(self.env.num_envs, self.num_steps_per_env,
+                              [self.env.num_obs], [self.env.num_privileged_obs],
+                              [self.env.num_actions])
 
-        # Log
+        # Logging
         self.log_dir = log_dir
         self.writer = None
+        self.wandb_run = None
         self.tot_timesteps = 0
         self.tot_time = 0
         self.current_learning_iteration = 0
 
         _, _ = self.env.reset()
-    
+
+    def _init_wandb(self):
+        """Initialize optional Weights & Biases logging for this training run."""
+        if not self.cfg.get("wandb_enabled", False) or self.wandb_run is not None:
+            return
+
+        if wandb is None:
+            raise RuntimeError(
+                "W&B logging is enabled but the `wandb` package is not installed. "
+                "Install it with `python -m pip install wandb`, or set "
+                "runner.wandb_enabled = False."
+            )
+
+        run_name = self.cfg.get("run_name", "")
+        if self.log_dir:
+            run_name = os.path.basename(os.path.normpath(self.log_dir))
+        if not run_name:
+            run_name = "himloco"
+
+        wandb_config = {
+            "runner": self.cfg,
+            "algorithm": self.alg_cfg,
+            "policy": self.policy_cfg,
+            "environment": {
+                "num_envs": self.env.num_envs,
+                "num_obs": self.env.num_obs,
+                "num_one_step_obs": self.env.num_one_step_obs,
+                "num_privileged_obs": self.env.num_privileged_obs,
+                "num_actions": self.env.num_actions,
+                "max_episode_length": int(self.env.max_episode_length),
+                "dt": float(self.env.dt),
+            },
+        }
+
+        self.wandb_run = wandb.init(
+            project=self.cfg.get("wandb_project", "HIMLoco"),
+            entity=self.cfg.get("wandb_entity", None),
+            name=run_name,
+            group=self.cfg.get("wandb_group", None),
+            tags=self.cfg.get("wandb_tags", None),
+            mode=self.cfg.get("wandb_mode", "online"),
+            dir=self.log_dir,
+            config=wandb_config,
+            save_code=True,
+        )
+
     def learn(self, num_learning_iterations, init_at_random_ep_len=False):
-        # initialize writer
+        # initialize loggers
         if self.log_dir is not None and self.writer is None:
             self.writer = SummaryWriter(log_dir=self.log_dir, flush_secs=10)
+        self._init_wandb()
+
         if init_at_random_ep_len:
-            self.env.episode_length_buf = torch.randint_like(self.env.episode_length_buf, high=int(self.env.max_episode_length))
+            self.env.episode_length_buf = torch.randint_like(
+                self.env.episode_length_buf,
+                high=int(self.env.max_episode_length),
+            )
         obs = self.env.get_observations()
         privileged_obs = self.env.get_privileged_observations()
         critic_obs = privileged_obs if privileged_obs is not None else obs
@@ -111,7 +170,12 @@ class HIMOnPolicyRunner:
                     obs, privileged_obs, rewards, dones, infos, termination_ids, termination_privileged_obs = self.env.step(actions)
 
                     critic_obs = privileged_obs if privileged_obs is not None else obs
-                    obs, critic_obs, rewards, dones = obs.to(self.device), critic_obs.to(self.device), rewards.to(self.device), dones.to(self.device)
+                    obs, critic_obs, rewards, dones = (
+                        obs.to(self.device),
+                        critic_obs.to(self.device),
+                        rewards.to(self.device),
+                        dones.to(self.device),
+                    )
                     termination_ids = termination_ids.to(self.device)
                     termination_privileged_obs = termination_privileged_obs.to(self.device)
 
@@ -119,7 +183,7 @@ class HIMOnPolicyRunner:
                     next_critic_obs[termination_ids] = termination_privileged_obs.clone().detach()
 
                     self.alg.process_env_step(rewards, dones, infos, next_critic_obs)
-                
+
                     if self.log_dir is not None:
                         # Book keeping
                         if 'episode' in infos:
@@ -138,7 +202,7 @@ class HIMOnPolicyRunner:
                 # Learning step
                 start = stop
                 self.alg.compute_returns(critic_obs)
-                
+
             mean_value_loss, mean_surrogate_loss, mean_estimation_loss, mean_swap_loss = self.alg.update()
             stop = time.time()
             learn_time = stop - start
@@ -147,15 +211,20 @@ class HIMOnPolicyRunner:
             if it % self.save_interval == 0:
                 self.save(os.path.join(self.log_dir, 'model_{}.pt'.format(it)))
             ep_infos.clear()
-        
+
         self.current_learning_iteration += num_learning_iterations
         self.save(os.path.join(self.log_dir, 'model_{}.pt'.format(self.current_learning_iteration)))
+
+        if self.wandb_run is not None:
+            self.wandb_run.finish()
+            self.wandb_run = None
 
     def log(self, locs, width=80, pad=35):
         self.tot_timesteps += self.num_steps_per_env * self.env.num_envs
         self.tot_time += locs['collection_time'] + locs['learn_time']
         iteration_time = locs['collection_time'] + locs['learn_time']
 
+        episode_metrics = {}
         ep_string = f''
         if locs['ep_infos']:
             for key in locs['ep_infos'][0]:
@@ -168,10 +237,14 @@ class HIMOnPolicyRunner:
                         ep_info[key] = ep_info[key].unsqueeze(0)
                     infotensor = torch.cat((infotensor, ep_info[key].to(self.device)))
                 value = torch.mean(infotensor)
-                self.writer.add_scalar('Episode/' + key, value, locs['it'])
-                ep_string += f"""{f'Mean episode {key}:':>{pad}} {value:.4f}\n"""
+                value_float = value.item()
+                episode_metrics['Episode/' + key] = value_float
+                self.writer.add_scalar('Episode/' + key, value_float, locs['it'])
+                ep_string += f"""{f'Mean episode {key}:':>{pad}} {value_float:.4f}\n"""
+
         mean_std = self.alg.actor_critic.std.mean()
-        fps = int(self.num_steps_per_env * self.env.num_envs / (locs['collection_time'] + locs['learn_time']))
+        fps = int(self.num_steps_per_env * self.env.num_envs /
+                  (locs['collection_time'] + locs['learn_time']))
 
         self.writer.add_scalar('Loss/value_function', locs['mean_value_loss'], locs['it'])
         self.writer.add_scalar('Loss/surrogate', locs['mean_surrogate_loss'], locs['it'])
@@ -182,48 +255,67 @@ class HIMOnPolicyRunner:
         self.writer.add_scalar('Perf/total_fps', fps, locs['it'])
         self.writer.add_scalar('Perf/collection time', locs['collection_time'], locs['it'])
         self.writer.add_scalar('Perf/learning_time', locs['learn_time'], locs['it'])
+
+        mean_reward = None
+        mean_episode_length = None
         if len(locs['rewbuffer']) > 0:
-            self.writer.add_scalar('Train/mean_reward', statistics.mean(locs['rewbuffer']), locs['it'])
-            self.writer.add_scalar('Train/mean_episode_length', statistics.mean(locs['lenbuffer']), locs['it'])
-            self.writer.add_scalar('Train/mean_reward/time', statistics.mean(locs['rewbuffer']), self.tot_time)
-            self.writer.add_scalar('Train/mean_episode_length/time', statistics.mean(locs['lenbuffer']), self.tot_time)
+            mean_reward = statistics.mean(locs['rewbuffer'])
+            mean_episode_length = statistics.mean(locs['lenbuffer'])
+            self.writer.add_scalar('Train/mean_reward', mean_reward, locs['it'])
+            self.writer.add_scalar('Train/mean_episode_length', mean_episode_length, locs['it'])
+            self.writer.add_scalar('Train/mean_reward/time', mean_reward, self.tot_time)
+            self.writer.add_scalar('Train/mean_episode_length/time', mean_episode_length, self.tot_time)
+
+        if self.wandb_run is not None:
+            wandb_metrics = {
+                'Loss/value_function': float(locs['mean_value_loss']),
+                'Loss/surrogate': float(locs['mean_surrogate_loss']),
+                'Loss/estimation': float(locs['mean_estimation_loss']),
+                'Loss/swap': float(locs['mean_swap_loss']),
+                'Loss/learning_rate': float(self.alg.learning_rate),
+                'Policy/mean_noise_std': float(mean_std.item()),
+                'Perf/total_fps': fps,
+                'Perf/collection_time': float(locs['collection_time']),
+                'Perf/learning_time': float(locs['learn_time']),
+                'Perf/iteration_time': float(iteration_time),
+                'Perf/total_time': float(self.tot_time),
+                'Train/total_timesteps': int(self.tot_timesteps),
+            }
+            if mean_reward is not None:
+                wandb_metrics['Train/mean_reward'] = float(mean_reward)
+                wandb_metrics['Train/mean_episode_length'] = float(mean_episode_length)
+            wandb_metrics.update(episode_metrics)
+            self.wandb_run.log(wandb_metrics, step=locs['it'])
 
         str = f" \033[1m Learning iteration {locs['it']}/{self.current_learning_iteration + locs['num_learning_iterations']} \033[0m "
 
         if len(locs['rewbuffer']) > 0:
             log_string = (f"""{'#' * width}\n"""
                           f"""{str.center(width, ' ')}\n\n"""
-                          f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs[
-                            'collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
+                          f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs['collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
                           f"""{'Value function loss:':>{pad}} {locs['mean_value_loss']:.4f}\n"""
                           f"""{'Surrogate loss:':>{pad}} {locs['mean_surrogate_loss']:.4f}\n"""
                           f"""{'Estimation loss:':>{pad}} {locs['mean_estimation_loss']:.4f}\n"""
                           f"""{'Swap loss:':>{pad}} {locs['mean_swap_loss']:.4f}\n"""
                           f"""{'Mean action noise std:':>{pad}} {mean_std.item():.2f}\n"""
-                          f"""{'Mean reward:':>{pad}} {statistics.mean(locs['rewbuffer']):.2f}\n"""
-                          f"""{'Mean episode length:':>{pad}} {statistics.mean(locs['lenbuffer']):.2f}\n""")
-                        #   f"""{'Mean reward/step:':>{pad}} {locs['mean_reward']:.2f}\n"""
-                        #   f"""{'Mean episode length/episode:':>{pad}} {locs['mean_trajectory_length']:.2f}\n""")
+                          f"""{'Mean reward:':>{pad}} {mean_reward:.2f}\n"""
+                          f"""{'Mean episode length:':>{pad}} {mean_episode_length:.2f}\n""")
         else:
             log_string = (f"""{'#' * width}\n"""
                           f"""{str.center(width, ' ')}\n\n"""
-                          f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs[
-                            'collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
+                          f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs['collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
                           f"""{'Value function loss:':>{pad}} {locs['mean_value_loss']:.4f}\n"""
                           f"""{'Surrogate loss:':>{pad}} {locs['mean_surrogate_loss']:.4f}\n"""
                           f"""{'Estimation loss:':>{pad}} {locs['mean_estimation_loss']:.4f}\n"""
                           f"""{'Swap loss:':>{pad}} {locs['mean_swap_loss']:.4f}\n"""
                           f"""{'Mean action noise std:':>{pad}} {mean_std.item():.2f}\n""")
-                        #   f"""{'Mean reward/step:':>{pad}} {locs['mean_reward']:.2f}\n"""
-                        #   f"""{'Mean episode length/episode:':>{pad}} {locs['mean_trajectory_length']:.2f}\n""")
 
         log_string += ep_string
         log_string += (f"""{'-' * width}\n"""
                        f"""{'Total timesteps:':>{pad}} {self.tot_timesteps}\n"""
                        f"""{'Iteration time:':>{pad}} {iteration_time:.2f}s\n"""
                        f"""{'Total time:':>{pad}} {self.tot_time:.2f}s\n"""
-                       f"""{'ETA:':>{pad}} {self.tot_time / (locs['it'] + 1) * (
-                               locs['num_learning_iterations'] - locs['it']):.1f}s\n""")
+                       f"""{'ETA:':>{pad}} {self.tot_time / (locs['it'] + 1) * (locs['num_learning_iterations'] - locs['it']):.1f}s\n""")
         print(log_string)
 
     def save(self, path, infos=None):
