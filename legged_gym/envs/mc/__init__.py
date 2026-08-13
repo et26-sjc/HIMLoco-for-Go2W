b020b89b8f3b13994eba6bcdcb4905a9b08f0177
Jingchao She <163161756+et26-sjc@users.noreply.github.com>
2026-08-13 13:55:28 +0800

    Export quiet MC evaluation task

--- legged_gym/envs/mc/__init__.py
+++ legged_gym/envs/mc/__init__.py
@@ -1,4 +1,13 @@
 from .mc_config import MCRoughCfg, MCRoughCfgPPO
 from .mc_robot import MC
+from .quiet_mc_config import QuietMCCfg, QuietMCCfgPPO
+from .quiet_mc_robot import QuietMC
 
-__all__ = ["MC", "MCRoughCfg", "MCRoughCfgPPO"]
+__all__ = [
+    "MC",
+    "MCRoughCfg",
+    "MCRoughCfgPPO",
+    "QuietMC",
+    "QuietMCCfg",
+    "QuietMCCfgPPO",
+]
