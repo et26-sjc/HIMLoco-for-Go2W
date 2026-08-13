8142fffee752719cd989acbaf90d73b77c9cb38c
Jingchao She <163161756+et26-sjc@users.noreply.github.com>
2026-08-13 17:48:36 +0800

    Register deterministic MC quiet evaluation task

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -8,6 +8,7 @@ from legged_gym.envs.mc.mc_config import MCRoughCfg, MCRoughCfgPPO
 from .mc.mc_robot import MC
 from legged_gym.envs.mc.quiet_mc_config import QuietMCCfg, QuietMCCfgPPO
 from .mc.quiet_mc_robot import QuietMC
+from .mc.quiet_mc_v2_robot import QuietMCV2
 
 import os
 
@@ -16,3 +17,4 @@ from legged_gym.utils.task_registry import task_registry
 task_registry.register("go2w", Go2w, GO2WRoughCfg(), GO2WRoughCfgPPO())
 task_registry.register("mc", MC, MCRoughCfg(), MCRoughCfgPPO())
 task_registry.register("quiet_mc", QuietMC, QuietMCCfg(), QuietMCCfgPPO())
+task_registry.register("quiet_mc_v2", QuietMCV2, QuietMCCfg(), QuietMCCfgPPO())
