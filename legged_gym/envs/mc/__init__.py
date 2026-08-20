1be4ca4986c1f52dc028c703dd98bf4ec7c1f362
Jingchao She <163161756+et26-sjc@users.noreply.github.com>
2026-08-20 22:39:23 +0800

    Export minimal 100 Hz MC configs

--- legged_gym/envs/mc/__init__.py
+++ legged_gym/envs/mc/__init__.py
@@ -1,5 +1,6 @@
 from .mc_config import MCRoughCfg, MCRoughCfgPPO
 from .mc_robot import MC
+from .mc_100hz_config import MC100HzCfg, MC100HzCfgPPO
 from .quiet_mc_config import QuietMCCfg, QuietMCCfgPPO
 from .quiet_mc_robot import QuietMC
 
@@ -7,6 +8,8 @@ __all__ = [
     "MC",
     "MCRoughCfg",
     "MCRoughCfgPPO",
+    "MC100HzCfg",
+    "MC100HzCfgPPO",
     "QuietMC",
     "QuietMCCfg",
     "QuietMCCfgPPO",
